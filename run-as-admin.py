eba7e23cdf785becea7dd9fb85c8aeb21061cca1
Adam Džadoň <adam.dzadon.gmail.com>
2022-01-21 21:19:13 +0100

    admin search add

---
+++ run-as-admin.py
@@ -0,0 +1,15 @@
+import ctypes, sys
+
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+
+if is_admin():
+    # Code of your program here
+    import dirtree_gui
+else:
+    # Re-run the program with admin rights
+    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
